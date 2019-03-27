fb157471821080bbf82d7d9458a7fc32e5a6783e
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-03-27 14:18:17 -0700

    commons-lang

---
+++ nondex/detCheck_commonslang.sh
@@ -0,0 +1,14 @@
+cd commons-lang
+git submodule init
+git submodule update
+git checkout fad946a1d
+cp ../../nondex_det/commons-lang/pom.xml ./
+cp ../../nondex_det/commons-lang/commons-lang/CompareToBuilder.java src/main/java/org/apache/commons/lang3/builder/
+cp ../../nondex_det/commons-lang/EqualsBuilder.java src/main/java/org/apache/commons/lang3/builder/
+cp ../../nondex_det/commons-lang/HashCodeBuilder.java src/main/java/org/apache/commons/lang3/builder/
+cp ../../nondex_det/commons-lang/ReflectionToStringBuilder.java src/main/java/org/apache/commons/lang3/builder/
+cp ../../nondex_det/commons-lang/FieldUtils.java src/main/java/org/apache/commons/lang3/reflect/
+mvn clean
+mvn compile > detOut
+echo "Number of errors detected by determinism checker:"
+grep -wr "\[ERROR\] " detOut | grep "java" |  wc -l
