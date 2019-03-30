ca40bfb0ada80c861f3b7842642b184dcc429a1e
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-03-29 18:13:18 -0700

    slf4j

--- nondex/detCheck_slf4j.sh
+++ nondex/detCheck_slf4j.sh
@@ -3,6 +3,7 @@ git submodule init
 git submodule update
 git checkout 52fcbbe8
 cp ../../nondex_det/slf4j/pom.xml ./
+cp ../../nondex_det/slf4j/SubstitutableLoggerTest.java slf4j-api/src/test/java/org/slf4j/helpers/
 mvn clean
 mvn compile > detOut
 echo "Number of errors detected by determinism checker:"
