fc94f3b27f5e58e04d9933b8acef3bbdcaa8518f
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-03-29 18:43:14 -0700

    ActionGenerator

--- nondex/detCheck_ActionGenerator.sh
+++ nondex/detCheck_ActionGenerator.sh
@@ -3,6 +3,7 @@ git submodule init
 git submodule update
 git checkout 10f4a3e
 cp ../../nondex_det/ActionGenerator/pom.xml ./
+cp ../../nondex_det/ActionGenerator/DataDictionaryEventSource.java ag-player/src/main/java/com/sematext/ag/source/dictionary/
 mvn clean
 mvn compile > detOut
 echo "Number of errors detected by determinism checker:"
